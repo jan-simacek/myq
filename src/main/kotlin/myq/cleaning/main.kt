ba1da6bf254857db9f5aff52149236168a436164
Jan Simacek <jsimacek@essentialaccessibility.com>
2022-06-22 21:23:26 +0200

    fixing tests, improving handling of the cmdline arguments

--- src/main/kotlin/myq/cleaning/main.kt
+++ src/main/kotlin/myq/cleaning/main.kt
@@ -1,13 +1,36 @@
 import kotlinx.serialization.ExperimentalSerializationApi
 import myq.cleaning.parser.Serializer
 import java.io.File
+import java.io.IOException
 import java.lang.IllegalArgumentException
 
-fun printUsage() {
-    println("Usage: main <input_file_path> <output_file_path>")
+/**
+ * Verifies if a writeable file can be open or created in 'path'. The complication stems from the fact, that there
+ * is no way of verifying if a writeable file can be created in a path without actually creating it.
+ */
+internal fun isPathWriteable(path: String): Boolean {
+    if (File(path).canWrite()) {
+        return true
+    }
+
+    try {
+        File(path).createNewFile()
+    } catch (e: IOException) {
+        return false
+    }
+    return true
 }
 
-fun validateArgs(args: Array<String>) {
+/**
+ * validates command line arguments
+ * @param args expects 2 arguments: first is a path of a readable JSON file with input, second is a path where a
+ * writeable file either exists or can be created, into which output will be written.
+ */
+internal fun validateArgs(args: Array<String>) {
+    fun printUsage() {
+        println("Usage: main <input_file_path> <output_file_path>")
+    }
+
     if (args.size != 2) {
         printUsage()
         throw IllegalArgumentException("2 arguments expected")
@@ -18,7 +41,7 @@ fun validateArgs(args: Array<String>) {
         throw IllegalArgumentException("First argument is not an existing and readable file: ${args[0]}")
     }
 
-    if (!File(args[1]).canWrite()) {
+    if (!isPathWriteable(args[1])) {
         printUsage()
         throw IllegalArgumentException("Second argument is not a writeable path: ${args[1]}")
     }
